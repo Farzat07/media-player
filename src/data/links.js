8d8d982431fe78509ac8464c0ce1b1815a3a066f
Abdul Rahman Farzat <A.Farzat@protonmail.com>
2021-04-09 10:41:46 +0900

    Define links in a separate file for consistency

---
+++ src/data/links.js
@@ -0,0 +1,22 @@
+export const links = [
+  {
+    id: 0,
+    url: "/",
+    text: "Home",
+  },
+  {
+    id: 1,
+    url: "/browser/",
+    text: "Browser",
+  },
+  {
+    id: 2,
+    url: "/history",
+    text: "History",
+  },
+  {
+    id: 3,
+    url: "/settings",
+    text: "Settings",
+  },
+];
